606054f2441fa80171dda0e235838166bcc0b0a5
Daniel Wegmann <daniel.wegmann@gmail.com>
2019-11-19 21:55:02 +0100

    lab 3-10

--- 03_more_datatypes/2_lists/03_10_unique.py
+++ 03_more_datatypes/2_lists/03_10_unique.py
@@ -6,3 +6,12 @@ unique_list = [55, 'hi', 4, 13]
 
 
 '''
+
+list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
+unique_list = []
+
+for el in list_:
+    if list_.count(el) == 1:
+        unique_list.append(el)
+
+print(unique_list)
