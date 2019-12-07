a26b09e50df8c781f899204f1571550f175ef42f
Daniel Wegmann <daniel.wegmann@gmail.com>
2019-12-07 22:26:59 +0100

    split

--- 03_more_datatypes/2_lists/03_11_split.py
+++ 03_more_datatypes/2_lists/03_11_split.py
@@ -4,3 +4,17 @@ create a list of all the words in the string and print the word with the most
 occurrences.
 
 '''
+
+string = input("input a string")
+
+words_list = string.split()
+occurences = {}
+for word in words_list:
+    occurences[word] = words_list.count(word)
+
+print(occurences)
+print(max(occurences, key=lambda key: occurences[key]))
+
+
+
+
